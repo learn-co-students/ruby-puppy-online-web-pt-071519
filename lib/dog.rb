84a3de38876cfda630844ad5969c4bf29c22d58e
Jenny O'Hara <jenny.s.ohara@gmail.com>
2019-07-24 15:21:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
+require 'pry'
 # Add your code here
+class Dog 
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end   
+  
+  def self.print_all
+    @@all.map {|dog| puts dog.name}
+  end  
+  
+  def self.clear_all
+    @@all.clear
+  end  
+end 
