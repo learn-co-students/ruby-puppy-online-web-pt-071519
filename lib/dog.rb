c8de4145785145841fce002e928aa1dcbe3d4db3
LaNice Powell <lanicemp@aol.com>
2019-08-08 18:44:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+require "pry"
+
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+    
+  end 
+  
+  def self.all 
+   @@all.collect do |dog_object|
+       dog_object.name
+        return @@all 
+     #For this method the  @@all alone also works, but Im trying to get comfortable with iterations.
+   end 
+  end 
+  
+  def self.clear_all
+    @@all.clear 
+  end 
+  
+  def self.print_all
+  self.all.each{|dog| puts "#{dog.name}"}
+  end 
+  
+  def save
+    #@@all << save  
+    
+  end 
+  Dog.new() 
+end
