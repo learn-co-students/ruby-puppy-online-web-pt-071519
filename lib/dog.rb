0b9920c5a4a46d8576a8ef037376caf7637eb8c8
Renee Sims <info@reneesoul.com>
2019-08-17 10:41:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,42 @@
-# Add your code here
+require 'pry'
+
+# Learning Goals
+# Use a class variable to keep track of every instance of a class that is created.
+# Write a class method to access the list of all instances of a class, stored in a class variable.
+
+class Dog
+  
+  attr_accessor :name, :dogs_names, :self, :print_all, :clear_all
+  attr_writer :save
+
+  def initialize(name)
+    @name = name
+    save
+
+  end
+  
+  @@all = []
+  @@dogs_names = []
+  
+
+  def self.all            # added attr_accessor
+    @@all
+    
+  end
+
+  def self.print_all      # added attr_accessor   
+    @@all.each {|dogs| @@dogs_names << dogs.name}
+    puts @@dogs_names     # used .name notation to                           # puts dog name instead of                         # the Dog instance object id.
+    
+  end
+  
+  def save                # added attr_writer
+    @@all << self
+
+  end
+
+  def self.clear_all      # added attr_accessor
+    @@clear = @@all.clear
+  end
+  
+end
