d708e7a2ba36fe23da96d903eb30f86474f01234
assane savadogo <assane.savadogo81@gmail.com>
2019-08-28 14:33:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,26 @@
 require 'pry'
 class Dog 
   attr_accessor :name
-  attr_reader :save
   @@all = []
-  
-  def initialize(name)
-    @name = name 
-    #@@all << self
-    self.save
-  end
-  
-  def self.all 
-    @@all 
-  end
-  
-  def self.print_all 
-    @@all.each do |dog|
-      puts dog.name 
-    end
-  end
-  
-  def save 
-    @@all << self 
-  end
-  
-  def self.clear_all 
-    @@all.clear 
-  end 
+ 
+ def initialize(name)
+   @name = name 
+   self.save
+ end
+ 
+ def self.all 
+   @@all 
+ end
+ 
+def self.clear_all
+  @@all.clear 
+end
+ 
+ def self.print_all 
+   Dog.all.each {|dog| puts dog.name}
+ end
+ 
+ def save 
+   @@all << self
+ end 
 end
